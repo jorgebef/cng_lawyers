ce618bfc899eca7546ff2ba368a832ea7492cbe4
Jorge Befan <jorge.befan@gmail.com>
2022-04-16 01:16:09 +0200

    create mantine theme file and add custom values

---
+++ styles/theme.ts
@@ -0,0 +1,22 @@
+import { MantineProviderProps, MantineThemeOverride } from '@mantine/core'
+
+export const theme: MantineThemeOverride = {
+  colorScheme: 'light',
+  primaryColor: 'blueish',
+  colors: {
+    blueish: ['#226093', '#7DA0C3'],
+  },
+  spacing: {
+    lg: 28,
+    xl: 40,
+  },
+  fontSizes: {
+    lg: 20,
+    xl: 26,
+  },
+  defaultRadius: 'md',
+  other: {
+    headerH: 60,
+    footerH: 90,
+  },
+}
