0631cd95316d90e007ab8c319243b58d9e3a8f4d
Jorge Befan <jorge.befan@gmail.com>
2022-04-18 13:00:08 +0200

    tweaking layout component to avoid unneeded padding

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,4 +1,10 @@
-import { AppShell, Container, createStyles } from '@mantine/core'
+import {
+  AppShell,
+  Box,
+  Container,
+  createStyles,
+  useMantineTheme,
+} from '@mantine/core'
 import { Footer } from '../footer'
 import { Header } from '../header'
 
@@ -6,19 +12,21 @@ type LayoutProps = {
   children: React.ReactNode
 }
 
-const useStyles = createStyles(theme => ({
-  layoutContainer: {
-    padding: `${theme.spacing.xl}px 0`,
-    minHeight: `calc(100vh - ${theme.other.footerH}px - ${theme.other.headerH}px)`,
-  },
-}))
-
 export const Layout = ({ children }: LayoutProps) => {
-  const { classes } = useStyles()
+  const theme = useMantineTheme()
 
   return (
     <AppShell header={<Header />} footer={<Footer />} padding={0}>
-      <Container className={classes.layoutContainer}>{children}</Container>
+      <Container
+        px={0}
+        pb={theme.spacing.xl}
+        size='xl'
+        sx={{
+          minHeight: `calc(100vh - ${theme.other.footerH}px - ${theme.other.headerH}px)`,
+        }}
+      >
+        {children}
+      </Container>
     </AppShell>
   )
 }
