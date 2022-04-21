b396bf0747c154e9881e3501a9f2556344f82a0a
Jorge Befan <jorge.befan@gmail.com>
2022-04-21 22:57:48 +0200

    redo Layout so that hero is only visible in index page

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,23 +1,42 @@
-import { AppShell, Box, useMantineTheme } from '@mantine/core'
+import {
+  AppShell,
+  Container,
+  createStyles,
+  useMantineTheme,
+} from '@mantine/core'
+import { useRouter } from 'next/router'
 import { Footer } from '../footer'
 import { Header } from '../header'
+import { Hero } from '../hero'
 
 type LayoutProps = {
   children: React.ReactNode
 }
 
+const useStyles = createStyles(theme => ({
+  appWrapper: {
+    marginTop: theme.other.headerH,
+    minHeight: `calc(100vh - ${theme.other.footerH}px - ${theme.other.headerH}px)`,
+  },
+
+  pageContainer: {
+    maxWidth: theme.breakpoints.xl,
+  },
+}))
+
 export const Layout = ({ children }: LayoutProps) => {
   const theme = useMantineTheme()
+  const router = useRouter()
+  const { classes } = useStyles()
 
   return (
     <AppShell header={<Header />} footer={<Footer />} padding={0}>
-      <Box
-        sx={{
-          minHeight: `calc(100vh - ${theme.other.footerH}px - ${theme.other.headerH}px)`,
-        }}
-      >
-        {children}
-      </Box>
+      <div className={classes.appWrapper}>
+        {router.asPath === '/' && <Hero />}
+        <Container py={theme.spacing.xl} className={classes.pageContainer}>
+          {children}
+        </Container>
+      </div>
     </AppShell>
   )
 }
